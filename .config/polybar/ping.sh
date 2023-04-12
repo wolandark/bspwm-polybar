2b3b37f95bf6ecd12da69afe1a05e70d73489092
wolandark <107309764+wolandark@users.noreply.github.com>
2023-04-12 05:15:43 +0330

    push config

---
+++ .config/polybar/ping.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ip=8.8.8.8
+ping_out=$(ping -c 1 "$ip" | awk 'NR==2{print $7}' | sed 's/time=//g')
+
+
+# while true; do
+#     ping -c 1 "$ip" | awk 'NR==2{print $7}' | sed 's/time=//g'
+#     sleep 30
+# done
+echo "$ping_out"
